0d2e9fdcaa2e0b51d6617ec7eacd5e9e4fc9e8d1
Henry Kafuuma <kafuuma>
2024-10-03 11:37:24 +0300

    add s3 and CDN

--- settings/settings.py
+++ settings/settings.py
@@ -144,10 +144,15 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_S3_ADDRESSING_STYLE= os.environ.get('AWS_S3_ADDRESSING_STYLE')
+AWS_S3_CUSTOM_DOMAIN=os.environ.get('AWS_S3_CUSTOM_DOMAIN')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# import pdb; pdb.set_trace()
 
 
